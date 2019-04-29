5258b13a14aef327712f68bf833a72b1482e82dc
can agriboz <agriboz@gmail.com>
2019-04-29 16:55:10 +0300

    add: Heatmaps

---
+++ components/Heatmaps/index.vue
@@ -0,0 +1,115 @@
+<template>
+  <div v-if="hasResponse" class="heatmap-wrapper">
+    <div class="d-flex justify-content-center">
+      <h5>{{ selectedHeatmap.name }}</h5>
+      <h5 v-if="!$moment(data.startDate).isSame($moment(data.endDate), 'year')">
+        - ( {{ $moment(data.startDate).format('YYYY') }} -
+        {{ $moment(data.endDate).format('YYYY') }} )
+      </h5>
+      <h5 v-else>-Along {{ $moment(data.startDate).format('YYYY') }}</h5>
+    </div>
+    <div v-for="(t, i) in data.days" :key="i" class="heatmap">
+      <div
+        v-if="ignoreDays[0].day !== t.day && ignoreDays[1].day !== t.day"
+        class="item"
+      >
+        <div class="item-days">
+          {{ t.day }}
+        </div>
+        <div
+          v-for="tt in t.data"
+          :key="tt.number"
+          v-b-tooltip.hover
+          :title="t.day"
+          :style="{
+            backgroundColor: setBackground(tt[selectedHeatmap.key])
+          }"
+          class="item-numbers"
+        >
+          <span v-if="isAverageKey">
+            {{
+              $moment
+                .duration(tt.avg_queue_time_weighted_average, 'seconds')
+                .format('mm:ss', { trim: false })
+            }}
+          </span>
+          <span v-else> {{ tt[selectedHeatmap.key] }}% </span>
+        </div>
+      </div>
+    </div>
+    <div class="item item-border">
+      <div class="item-days" />
+      <template v-for="(t, i) in data.days[0].data">
+        <div :key="i" class="item-numbers">{{ t.hour }}</div>
+      </template>
+    </div>
+    <div class="item-perc-wrapper d-flex justify-content-center">
+      <div class="item-perc">
+        <div class="item-inner">
+          <div class="perc-block orange" />
+          <span v-if="isAverageKey">
+            {{ threshold.threshold2 }} - {{ threshold.threshold1 }}
+          </span>
+          <span v-else>
+            % {{ +threshold.threshold2 + 1 }} - % {{ threshold.threshold1 }}
+          </span>
+        </div>
+      </div>
+      <div class="item-perc">
+        <div class="item-inner">
+          <div class="perc-block red" />
+          <span v-if="isAverageKey">
+            {{ threshold.threshold1 }} - 00:59:59
+          </span>
+          <span v-else> % 0 - % {{ threshold.threshold2 }} </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      required: true,
+      type: Object
+    },
+    threshold: {
+      required: true,
+      type: Object
+    },
+    selectedHeatmap: {
+      required: true,
+      type: Object
+    },
+    ignoreDays: {
+      required: true,
+      type: Array
+    },
+    hasResponse: {
+      required: true,
+      type: Boolean
+    }
+  },
+
+  computed: {
+    isAverageKey() {
+      return (
+        this.selectedHeatmap.key === 'avg_queue_time_weighted_average' ||
+        this.selectedHeatmap.key === 'avg_handle_time_weighted_average'
+      )
+    }
+  },
+
+  methods: {
+    setBackground(data) {
+      return data <= this.threshold.threshold2
+        ? '#fa4a4a'
+        : data <= this.threshold.threshold1
+        ? '#ff7e30'
+        : '#29af39'
+    }
+  }
+}
+</script>
---
+++ pages/heatmaps/index.vue
@@ -0,0 +1,342 @@
+<template>
+  <div class="container-fluid">
+    <!-- start page title -->
+    <page-title title="Heatmaps" />
+    <!-- end page title -->
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-body">
+            <div v-if="!hasResponse" class="row">
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label>Heatmap List</label>
+                  <select v-model="selectedHeatmap" class="form-control">
+                    <option v-for="h in heatmapList" :key="h.id" :value="h">
+                      {{ h.name }}
+                    </option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <report-filter
+              v-if="!hasResponse"
+              :show-footer="false"
+              :is-searchable="false"
+              :url="'/teamnotreadypercentage'"
+              :item="item"
+            />
+            <!-- <div v-if="!hasResponse" class="form-group">
+              <div class="row">
+                <div class="col-md-6">
+                  <label>Shift Start Time</label>
+                  <select v-model="item.shift.start" class="form-control">
+                    <option v-for="s in range(23, true)" :key="s" :value="s">{{
+                      s
+                    }}</option>
+                  </select>
+                </div>
+                <div class="col-md-6">
+                  <label>Shift End Time</label>
+                  <select v-model="item.shift.end" class="form-control">
+                    <option v-for="s in range(23, true)" :key="s" :value="s">{{
+                      s
+                    }}</option>
+                  </select>
+                </div>
+              </div>
+            </div> -->
+            <div class="form-group">
+              <button
+                v-if="!hasResponse"
+                class="btn btn-primary mr-1"
+                @click="search"
+              >
+                Search
+              </button>
+              <button
+                v-if="hasResponse"
+                class="btn btn-warning"
+                @click="hasResponse = !hasResponse"
+              >
+                Filter
+              </button>
+              <button
+                v-if="hasResponse"
+                class="btn btn-info ml-1"
+                @click="openThresholdModal = true"
+              >
+                Change Threshold
+              </button>
+              <button
+                v-if="hasResponse"
+                class="btn btn-primary ml-1"
+                @click="downloadAsPng"
+              >
+                Download as PNG
+              </button>
+              <button
+                v-if="hasResponse"
+                class="btn btn-primary ml-1"
+                @click="ignoreWeekends('saturday')"
+              >
+                {{
+                  !ignoreDays[0].ignored ? 'Show Saturday' : 'Ignore Saturday'
+                }}
+              </button>
+              <button
+                v-if="hasResponse"
+                class="btn btn-primary ml-1"
+                @click="ignoreWeekends('sunday')"
+              >
+                {{ !ignoreDays[1].ignored ? 'Show Sunday' : 'Ignore Sunay' }}
+              </button>
+            </div>
+            <component
+              :is="heatmapComponent"
+              :threshold="threshold"
+              :data="heatmapDetail"
+              :has-response="hasResponse"
+              :selected-heatmap="selectedHeatmap"
+              :ignore-days="ignoreDays"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <b-modal v-model="openThresholdModal" title="Change Threshold">
+      <section>
+        <div v-if="heatmapDetail" class="col-md-12">
+          <div class="form-group">
+            <label>Threshold 1: (Red)</label>
+            <input
+              v-model="threshold.threshold1"
+              type="text"
+              class="form-control"
+              placeholder="Please write the threshold value"
+            />
+          </div>
+          <div class="form-group">
+            <label>Threshold 2: (Orange)</label>
+            <input
+              v-model="threshold.threshold2"
+              type="text"
+              class="form-control"
+              placeholder="Please write the threshold value"
+            />
+          </div>
+        </div>
+      </section>
+    </b-modal>
+  </div>
+  <!-- container -->
+</template>
+
+<script>
+import PageTitle from '@/components/PageTitle'
+import ReportFilter from '@/components/StockReportFilter'
+
+import html2canvas from 'html2canvas'
+import dateRanges from '@/utils/dateRanges'
+import { mapActions } from 'vuex'
+
+export default {
+  layout: 'authenticated',
+  components: {
+    ReportFilter,
+    PageTitle
+  },
+  data: () => ({
+    hasResponse: false,
+    heatmapDetail: null,
+    item: {
+      csqs: [],
+      startTime: dateRanges(1, true, false),
+      endTime: dateRanges(1, false, true),
+      shift: {
+        start: '08:00',
+        end: '20:00'
+      }
+    },
+    threshold: {
+      threshold1: null,
+      threshold2: null
+    },
+    openThresholdModal: false,
+    ignoreDays: [
+      {
+        ignored: false,
+        day: 'Saturday'
+      },
+      {
+        ignored: false,
+        day: 'Sunday'
+      }
+    ],
+    selectedHeatmap: null,
+    heatmapComponent: null,
+    heatmapList: [
+      {
+        id: 1,
+        name: 'Call Answer Rate',
+        componentName: 'CallAnswerRate',
+        key: 'call_answer_rate'
+      },
+      {
+        id: 2,
+        name: 'Call Abandon Rate',
+        componentName: 'CallAbandonRate',
+        key: 'calls_abandoned_rate'
+      },
+      {
+        id: 3,
+        name: 'Average Wait Time',
+        componentName: 'AverageWaitTime',
+        key: 'avg_queue_time_weighted_average'
+      },
+      {
+        id: 4,
+        name: 'Average Handle Time',
+        componentName: 'AverageHandleTime',
+        key: 'avg_handle_time_weighted_average'
+      },
+      {
+        id: 5,
+        name: 'Service Level',
+        componentName: 'ServiceLevel',
+        key: 'service_level_rate'
+      },
+      {
+        id: 6,
+        name: 'Calls Presented',
+        componentName: 'CallsPresented',
+        key: 'calls_presented'
+      },
+      {
+        id: 7,
+        name: 'Calls Handled',
+        componentName: 'CallsHandled',
+        key: 'calls_handled'
+      },
+      {
+        id: 8,
+        name: 'Calls Abandoned',
+        componentName: 'CallsAbandoned',
+        key: 'calls_abandoned'
+      }
+    ],
+    title: 'Comstice Quartz - Heatmaps'
+  }),
+
+  head() {
+    return {
+      title: this.title
+    }
+  },
+
+  beforeMount() {
+    this.selectedHeatmap = this.heatmapList[0]
+  },
+
+  methods: {
+    ...mapActions({
+      getHeatMapDetail: 'heatmaps/getHeatMapDetail'
+    }),
+
+    ignoreWeekends(selectedDay) {
+      if (selectedDay === 'saturday') {
+        this.ignoreDays[0].ignored
+          ? (this.ignoreDays[0].day = 'Saturday')
+          : (this.ignoreDays[0].day = '')
+        this.ignoreDays[0].ignored
+          ? (this.ignoreDays[0].ignored = false)
+          : (this.ignoreDays[0].ignored = true)
+      }
+      if (selectedDay === 'sunday') {
+        this.ignoreDays[1].ignored
+          ? (this.ignoreDays[1].day = 'Sunday')
+          : (this.ignoreDays[1].day = '')
+        this.ignoreDays[1].ignored
+          ? (this.ignoreDays[1].ignored = false)
+          : (this.ignoreDays[1].ignored = true)
+      }
+    },
+
+    range(num, beginAtZero) {
+      const count = Array.from(
+        { length: beginAtZero ? num + 1 : num },
+        (v, k) => {
+          return beginAtZero
+            ? k < 10
+              ? '0' + (k + ':00')
+              : '' + (k + ':00')
+            : k + 1
+        }
+      )
+      return count
+    },
+
+    downloadAsPng() {
+      const node = document.getElementsByClassName('heatmap-wrapper')[0]
+      html2canvas(node).then(canvas => {
+        var link = document.createElement('a')
+        document.body.appendChild(link)
+        link.download = 'image.png'
+        link.href = canvas.toDataURL('image/png')
+        link.target = '_blank'
+        link.click()
+      })
+    },
+
+    async search() {
+      this.heatmapComponent = await null
+      this.hasResponse = await false
+      const payload = {
+        url: this.selectedHeatmap.name.replace(/ /gi, '-').toLowerCase(),
+        data: this.item
+      }
+      const { data } = await this.$axios.post(
+        `/heat-map/${payload.url}`,
+        payload.data
+      )
+      this.hasResponse = await true
+      this.heatmapDetail = await data
+
+      if (
+        // should refactor them codes. it is written in bad conditions!
+        this.selectedHeatmap.componentName === 'CallAnswerRate' ||
+        this.selectedHeatmap.componentName === 'ServiceLevel'
+      ) {
+        this.threshold.threshold1 = this.heatmapDetail.legend.orange
+        this.threshold.threshold2 = this.heatmapDetail.legend.red
+      } else if (this.selectedHeatmap.componentName === 'CallAbandonRate') {
+        this.threshold.threshold1 = this.heatmapDetail.legend.red
+        this.threshold.threshold2 = this.heatmapDetail.legend.orange
+      } else if (this.selectedHeatmap.componentName === 'AverageWaitTime') {
+        this.threshold.threshold1 = this.$moment
+          .duration(this.heatmapDetail.legend.red, 'seconds')
+          .format('mm:ss', { trim: false })
+        this.threshold.threshold2 = this.$moment
+          .duration(this.heatmapDetail.legend.orange, 'seconds')
+          .format('mm:ss', { trim: false })
+      } else if (
+        this.selectedHeatmap.componentName === 'CallsPresented' ||
+        this.selectedHeatmap.componentName === 'CallsAbandoned' ||
+        this.selectedHeatmap.componentName === 'CallsHandled'
+      ) {
+        this.threshold.threshold1 = 0
+        this.threshold.threshold2 = 0
+      } else {
+        this.threshold.threshold1 = this.$moment
+          .duration(this.heatmapDetail.legend.red, 'seconds')
+          .format('mm:ss', { trim: false })
+        this.threshold.threshold2 = this.$moment
+          .duration(this.heatmapDetail.legend.orange, 'seconds')
+          .format('mm:ss', { trim: false })
+      }
+
+      this.heatmapComponent = async () => await import(`@/components/Heatmaps`)
+    }
+  }
+}
+</script>
